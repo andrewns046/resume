829dca74fb941616b6d60f529c286ae6dabf4791
Andrew Sanchez <andrew.sanchez046@gmail.com>
2019-11-01 11:46:27 -0700

    added timeline

--- scripts/animations.js
+++ scripts/animations.js
@@ -2,35 +2,41 @@ $(document).ready(function(){
 	$("#section1").fadeIn();
 	$("#section1").click(function() {
 		//todo: trigger section load
-		$("#objective").show();
+		$("#objective").show(500);
 	});
+
 	$("#section2").fadeIn(1000);
 	$("#section2").click(function() {
-		$("#education").show();
+		$("#education").show(500);
 	});
+	
 	$("#section3").fadeIn(2000);
 	$("#section3").click(function() {
 		//todo: trigger section load
-		$("#experience").show();
+		$("#experience").show(500);
 	});
+	
 	$("#section4").fadeIn(3000);
 	$("#section4").click(function() {
 		//todo: trigger section load
-		$("#projects").show();
+		$("#projects").show(500);
 	});
+	
 	$("#section5").fadeIn(4000);
 	$("#section5").click(function() {
 		//todo: trigger section load
-		$("#skills").show();
+		$("#skills").show(500);
 	});
+	
 	$("#section6").fadeIn(5000);
 	$("#section6").click(function() {
 		//todo: trigger section load
-		$("#associations").show();
+		$("#associations").show(500);
 	});
+	
 	$("#section7").fadeIn(5000);
 	$("#section7").click(function() {
 		//todo: trigger section load
-		$("#contact").show();
+		$("#contact").show(500);
 	});
 });
