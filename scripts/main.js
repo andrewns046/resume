6338ea3ccaaf257512843e9069ab0a1f63980a4d
Andrew Sanchez <andrew.sanchez046@gmail.com>
2019-11-09 04:07:22 -0800

    so tired, a lot of changes

--- scripts/main.js
+++ scripts/main.js
@@ -6,16 +6,24 @@ var sections = [ {sectionName:"objective", sectionSelector:"#objective", isLoade
 			     {sectionName:"skills" , sectionSelector:"#skills", isLoaded:false},
 			     {sectionName:"associations", sectionSelector:"#associations", isLoaded:false}];
 
+const numSections = 6;
+const logoName = "Andrew Sanchez";
 var numHolders = 6;  // number of section holders to load.
+var nextScrollY = 0;
 
 // when document is ready
 $(document).ready(function(){
 	var isMobile = false;
+	// make svg logo
+	makeLogo();
 
 	// Load pages under list items if on mobile
 	if (typeof window.orientation !== 'undefined') {
 		isMobile = true;
 	}
+
+	//prepare first scroll
+	nextScrollY = $(".jumbotron").outerHeight(true);
 
 	// add nav click listeners
 	var i = 0;
@@ -23,7 +31,6 @@ $(document).ready(function(){
 		addNavListener(sections[i], isMobile);
 	}
 
-	// contact info
 	$("#contact-link").click(function() {$("#contact-info").fadeIn(1000);});
 });
 
@@ -39,6 +46,9 @@ function addNavListener( sectionObject, isMobile ) {
 	$(sectionObject.sectionSelector+"-link").click(function() {
 		if(sectionObject.isLoaded == false) {  // if section is not loaded
 			sectionObject.isLoaded = cardLoad(sectionObject.sectionName, sectionObject.sectionSelector, isMobile);
+		} else { 
+			location.href = "#";
+			location.href = sectionObject.sectionSelector;
 		}		
 	});
 
@@ -63,25 +73,59 @@ function cardLoad(sectionName , sectionSelector, isMobile ) {
 		$("#mobile-section-"+sectionName).load(sectionHTMLPath);
 	}
 	else {
-		var result = 6 - numHolders;
+		var result = numSections - numHolders;
 		loadLocator = "#holder"+result;
 		numHolders = numHolders - 1;  //update sections left
 		$(loadLocator).load(sectionHTMLPath, function(){
 			// once loaded fade in section
-			$(loadLocator).fadeIn(1000);
+			$(loadLocator).fadeIn(2000);
+			//scroll window past current section
+			$("html, body").animate({ scrollTop: nextScrollY }, "slow");
+			nextScrollY += ($(sectionSelector).outerHeight(true) + 40);
 		});
+
 		$(loadLocator).after("<br>");
 	}
 
 	return true;
 }
 
-/*
- * Fades in borders clockwise
- * Arguments:
- *     selector - (String) CSS selector
- */
-function borderAnimation( selector ) {
+// Dynamically draws logo
+function makeLogo() {
+	var widthNav = $("#navList").outerWidth(true);
+	var sf = scaleFactor( 320, widthNav);
+	var height = 71*sf;
+	var yText = 49*sf;
+	sf = scaleFactor( 71, height);
+	var sWidth = 18*sf;
+	var xText = 9*sf
+	var startX1 = widthNav-(sWidth*3);
+	var startX2 = startX1 + sWidth;
+	var startX3 = startX2 + sWidth;
+
+	svgHTML = `<svg id="logoCenter" width="${widthNav}" height="${height}">
+            <rect width="${startX1}" height="${height}" style="fill:rgb(5,5,5)" />
+            <rect x="${startX1}" width="${sWidth}" height="${height}" style="fill:rgb(0,162,229)" />
+            <rect x="${startX2}" width="${sWidth}" height="${height}" style="fill:rgb(230,22,16)" />
+            <rect x="${startX3}" width="${sWidth}" height="${height}" style="fill:rgb(254,251,100)" />
+            <text x="${xText}" y="${yText}" fill="white">${logoName}</text>
+            Sorry, your browser does not support inline SVG.
+          </svg>`;
+
+	$("#navList").before(svgHTML);
 
 }
 
+/* Scaling helper function
+ * Arguments:
+ *    lengthRef: (number) reference length of the object you want to scale
+ *    lengthTarget: (number) target length
+ */
+function scaleFactor( lengthRef, lengthTarget ) {
+	sf = lengthRef/lengthTarget;
+	if( lengthRef > lengthTarget ) {
+		return 1/sf;
+	} else {
+		return sf;
+	}
+}
