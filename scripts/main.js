675c021865cf5a8d948f1717b60aa61b9ef03474
Andrew Sanchez <andrew.sanchez046@gmail.com>
2019-11-12 15:52:51 -0800

    started transition to new layout

--- scripts/main.js
+++ scripts/main.js
@@ -1,101 +1,119 @@
 /*sections to load*/
-var sections = [ {sectionName:"objective", sectionSelector:"#objective", isLoaded:false}, 
-			     {sectionName:"education" ,sectionSelector:"#education", isLoaded:false},
-			     {sectionName:"experience" ,sectionSelector:"#experience", isLoaded:false},
-			     {sectionName:"projects", sectionSelector:"#projects", isLoaded:false},
-			     {sectionName:"skills" , sectionSelector:"#skills", isLoaded:false},
-			     {sectionName:"associations", sectionSelector:"#associations", isLoaded:false}];
+var sections = [ {sectionName:"Objective", sectionSelector:"#objective", isLoaded:false}, 
+			     {sectionName:"Education" ,sectionSelector:"#education", isLoaded:false},
+			     {sectionName:"Experience" ,sectionSelector:"#experience", isLoaded:false},
+			     {sectionName:"Projects", sectionSelector:"#projects", isLoaded:true},
+			     {sectionName:"Skills" , sectionSelector:"#skills", isLoaded:false},
+			     {sectionName:"Associations", sectionSelector:"#associations", isLoaded:false}];
 
 const numSections = 6;
+const fadeSpeed = 2000; // two second fades
 const logoName = "Andrew Sanchez";
-var numHolders = 6;  // number of section holders to load.
-var nextScrollY = 0;
+var lastActiveSectionSelector = "#projects";
+var toLoad = 6;  // number of sections left to load 
+
 
 // when document is ready
 $(document).ready(function(){
 	var isMobile = false;
+	
 	// make svg logo
 	makeLogo();
 
-	// Load pages under list items if on mobile
+	// mobile condition
 	if (typeof window.orientation !== 'undefined') {
 		isMobile = true;
 	}
 
-	//prepare first scroll
-	nextScrollY = $(".jumbotron").outerHeight(true);
+	//add listener to banner
+	$("#close-banner-btn").click(function() {
+		// remove welcome banner
+		$(".welcome-banner").hide(1000);
+	});
 
 	// add nav click listeners
 	var i = 0;
 	for(i = 0; i < sections.length; i++) {
-		addNavListener(sections[i], isMobile);
+		addNavListener(sections[i]);
 	}
 
-	$("#contact-link").click(function() {$("#contact-info").fadeIn(1000);});
+	// load launch page
+	pageLoad( sections[3] );
+
 });
 
 /*
  * adds listeners to nav buttons
  * Arguments:
  *     sectionObject - (Object) Contains section information
- *     isMobile - (Boolean) If true sections will be added as a new nav item, otherwise section is
- *                          added to placeholder
  */
-function addNavListener( sectionObject, isMobile ) {
+function addNavListener( sectionObject ) {
 	// query nav section list item
-	$(sectionObject.sectionSelector+"-link").click(function() {
-		if(sectionObject.isLoaded == false) {  // if section is not loaded
-			sectionObject.isLoaded = cardLoad(sectionObject.sectionName, sectionObject.sectionSelector, isMobile);
-		} else { 
-			location.href = "#";
-			location.href = sectionObject.sectionSelector;
-		}		
+	$(`${sectionObject.sectionSelector}-link`).click(function() {
+		if( lastActiveSectionSelector != sectionObject.sectionSelector) { 
+			if(sectionObject.isLoaded == false) {  // if section is not loaded
+				sectionObject.isLoaded = pageLoad(sectionObject);
+			} else {
+				pageTransition(sectionObject);
+			}
+		}
 	});
 }
 
 /*
- * loads html pages in bootstrap cards in the order they were requested
+ * Performs animations for nav clicks, once the html is loaded on the DOM.
  * Arguments:
- *     sectionName - (String) name of section. (ex objective, education, ...)
- *     sectionSelector - (String) #sectionName
- *     isMobile - (Boolean) If true sections will be added as a new nav item, otherwise section is
- *                          added to placeholder
+ *     sectionObject - (Object) Contains section information
  */
-function cardLoad(sectionName , sectionSelector, isMobile ) {
+function pageTransition( sectionObject ) {
+	// hide last section
+	$(lastActiveSectionSelector).hide('fast');
+	lastActiveSectionSelector = sectionObject.sectionSelector;
+	// update header
+	$(".page-header-text").text(sectionObject.sectionName);
+	// show selected section
+	$(sectionObject.sectionSelector).fadeIn(fadeSpeed);
+	// TODO: highlight selected section in navbar
+}
+
+
+/*
+ * loads html pages in the order they were requested
+ * Arguments:
+ *    sectionObject - (Object) Contains section information
+ */
+function pageLoad( sectionObject ) {
 	var loadLocator = "";
-	var sectionHTMLPath = "../" + sectionName +".html " + sectionSelector;
-	if( isMobile == true) {
-		//create new list item after section link 
-		$(sectionSelector+"-link").after("<div id='mobile-section-" + sectionName +"'></div>");
-		$("#mobile-section-"+sectionName).load(sectionHTMLPath);
-	}
-	else {
-		var result = numSections - numHolders;
-		loadLocator = "#holder"+result;
-		numHolders = numHolders - 1;  //update sections left
-		$(loadLocator).load(sectionHTMLPath, function(){
-			// once loaded fade in section
-			$(loadLocator).fadeIn(2000);
-			//scroll window past current section
-			$("html, body").animate({ scrollTop: nextScrollY }, "slow");
-			nextScrollY += ($(sectionSelector).outerHeight(true) + 40);
-		});
+	var sectionHTMLPath = "../" + String(sectionObject.sectionName).toLowerCase() +".html " + sectionObject.sectionSelector;
+	var result = numSections - toLoad;
+	loadLocator = "#holder" + result;
+	toLoad = toLoad - 1;  //update sections left
 
-		$(loadLocator).after("<br>");
-	}
+	$(loadLocator).load(sectionHTMLPath, function(){
+		// once loaded fade in section
+		$(loadLocator).fadeIn(fadeSpeed);
+		pageTransition(sectionObject);
+	});
 
+	$(loadLocator).after("<br>");
+	
 	return true;
 }
 
 // Dynamically draws logo
 function makeLogo() {
 	var widthNav = $("#navList").outerWidth(true);
+	// x scales
 	var sf = scaleFactor( 320, widthNav);
 	var height = 71*sf;
-	var yText = 49*sf;
+	var yText = 49*sf;  
+	
+	// y scales
 	sf = scaleFactor( 71, height);
+	var xText = 9*sf;
 	var sWidth = 18*sf;
-	var xText = 9*sf
+	
+	// x bar locations
 	var startX1 = widthNav-(sWidth*3);
 	var startX2 = startX1 + sWidth;
 	var startX3 = startX2 + sWidth;
@@ -109,7 +127,7 @@ function makeLogo() {
             Sorry, your browser does not support inline SVG.
           </svg>`;
 
-	$("#navList").before(svgHTML);
+	$("#logoContainer").html(svgHTML);
 
 }
 
